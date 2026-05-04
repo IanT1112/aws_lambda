9dfea1da2bdbd76ecf863475521c01cf9f800354
IanT1112 <iantapia1104@gmail.com>
2026-05-04 03:21:05 -0500

    feat(env): agregar entorno dev

---
+++ environments/dev/variables.tf
@@ -0,0 +1,28 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "suffix" {
+  description = "Unique suffix for globally unique S3 bucket name"
+  type        = string
+}
+
+variable "upload_lambda_memory" {
+  description = "Memory in MB for upload lambda"
+  type        = number
+  default     = 128
+}
+
+variable "crop_lambda_memory" {
+  description = "Memory in MB for crop lambda"
+  type        = number
+  default     = 256
+}
